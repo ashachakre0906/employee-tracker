ad11e09ce643eddf89ba2dd34c61289e27001d8b
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-19 00:05:37 -0700

    added queries and inserted values to the table

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,3 @@
-SELECT movies.movie_name AS movie, reviews.review
-FROM reviews
-LEFT JOIN movies
-ON reviews.movie_id = movies.id
-ORDER BY movies.movie_name;
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,29 @@
 INSERT INTO department (name)
-VALUES ("Engineering department");
+VALUES ('Engineering'),
+       ('Quality Assurance'),
+       ('Design'),
+       ('HR'),
+       ('Sales'),
+       ('Finance'),
+       ('Admin');
+ 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Software Engineer', 100000, 1);
+VALUES ('Software Engineer', 100000, 1),
+       ('QA Engineer', 8000, 2);
+       ('UI/UX Designer', 12000, 3),
+       ('Director',11000, 4),
+       ('Lead', 6000, 5),
+       ('Financial advisor',7000, 6),
+       ('Systems Administrator',3000, 7);
 
 INSERT INTO employee (first_name, last_name,role_id,manager_id)
-VALUES ('Bob', 'Billy', 1, NULL);
+VALUES ('Bob', 'Billy', 1, NULL),
+       ('Andrew', 'Ray', 2, 1),
+       ('Sandra', 'Will', 3, NULL),
+       ('Vanessa', 'Rodrigues', 4, 2),
+       ('Alex', 'Kraux', 5, NULL),
+       ('Andres','Paulson',6, 3),
+       ('Louis', 'David', 7 , NULL);
+       
        
